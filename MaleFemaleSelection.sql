b7f3e01ad13e60a992eaabef35f8ea53e0011357
HaileyRW <61215904+HaileyRW@users.noreply.github.com>
2020-07-20 21:16:45 -0400

    Update MaleFemaleSelection.sql

--- MaleFemaleSelection.sql
+++ MaleFemaleSelection.sql
@@ -1,3 +1,7 @@
-/*display male and female students*/
+/*display male and female students based on genderID*/
+select * from studentact.student where gender = 1;
+select * from studentact.student where gender = 2;
+
+/*display male and female students based on gender*/
 select * from studentact.student where gender = 'F';
 select * from studentact.student where gender = 'M';
