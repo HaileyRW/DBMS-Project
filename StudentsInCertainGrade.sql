7a03b8b5a054ea35e831063de1974bea18f8dff9
HaileyRW <61215904+HaileyRW@users.noreply.github.com>
2020-07-20 21:14:30 -0400

    Update StudentsInCertainGrade.sql

--- StudentsInCertainGrade.sql
+++ StudentsInCertainGrade.sql
@@ -1,8 +1,17 @@
-/*Display Students in a Certain Grade*/
-select * from studentact.student where grade = 6;
-select * from studentact.student where grade = 7;
-select * from studentact.student where grade = 8;
-select * from studentact.student where grade = 9;
-select * from studentact.student where grade = 10;
-select * from studentact.student where grade = 11;
-select * from studentact.student where grade = 12;
+/*Display Students in a Certain Grade using gradeID*/
+select * from studentact.student where gradeID = 06;
+select * from studentact.student where gradeID = 07;
+select * from studentact.student where gradeID = 08;
+select * from studentact.student where gradeID = 09;
+select * from studentact.student where gradeID = 010;
+select * from studentact.student where gradeID = 011;
+select * from studentact.student where gradeID = 012;
+
+/*Display Students in a Certain Grade using grade*/
+select * from studentact.student where gradeID = 6;
+select * from studentact.student where gradeID = 7;
+select * from studentact.student where gradeID = 8;
+select * from studentact.student where gradeID = 9;
+select * from studentact.student where gradeID = 10;
+select * from studentact.student where gradeID = 11;
+select * from studentact.student where gradeID = 12;
